2649b416148d419d7b3057c5a9992abea1eff753
moaoh <k010103@naver.com>
2023-07-10 23:25:17 +0900

    feat: :sparkles: 15486번 - 퇴사 2

---
+++ junmkang/BaekJoon/C++/15486번 - 퇴사 2/15486.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+int		main(void)
+{
+	int n, max = -1;
+	int tp[1600000][2] = {0,}, dp[1600000] = {0,};
+
+	std::cin >> n;
+	for (int i = 0; i < n; i++)
+		std::cin >> tp[i][0] >> tp[i][1];
+
+	for (int i = 0; i <= n; i++) {
+		int deadline = i + tp[i][0];
+		if (max > dp[i]) dp[i] = max;
+		if (deadline <= n)
+			dp[deadline] = std::max(dp[deadline], tp[i][1] + dp[i]);
+		if (dp[i] > max) max = dp[i];
+	}
+	std::cout << max;
+
+	return (0);
+}
