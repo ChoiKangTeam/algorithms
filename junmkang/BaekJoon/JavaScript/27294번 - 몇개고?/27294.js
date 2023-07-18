6620960fdc532e4ad37a66374a3c0acab6d25129
moaoh <k010103@naver.com>
2023-07-18 18:50:02 +0900

    feat: :sparkles: 27294번 - 몇개고?

---
+++ junmkang/BaekJoon/JavaScript/27294번 - 몇개고?/27294.js
@@ -0,0 +1,10 @@
+let [t, s] = require("fs")
+	.readFileSync('/dev/stdin')
+	.toString()
+	.trim()
+	.split(' ')
+	.map(Number);
+if (12 <= t && t <= 16 && s === 0) 
+	console.log("320");
+else 
+	console.log("280");
